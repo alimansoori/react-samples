60feec34df7dd1cba8986514c125b5c9d6c523ee
alimansoori <alimanssouri221@gmail.com>
2019-10-12 06:53:40 +0330

    music player set context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import TrackList from './app/music-player/components/TrackList';
+import { MusicPlayerProvider } from './app/music-player/MusicPlayerContext';
+
 function App() {
   return (
-    <>
-    </>
+    <MusicPlayerProvider>
+      <div className='container'>
+        <TrackList/>
+      </div>
+    </MusicPlayerProvider>
   );
 }
 
